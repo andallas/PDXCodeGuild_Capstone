22fcde9ce45be98d85b6e0f34ab7b6f52b0a6c91
Justin <justin.35f@gmail.com>
2022-04-11 18:30:16 -0700

    More work on profiles

--- apis/urls.py
+++ apis/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, UserInfoViewSet
 from rest_framework.routers import DefaultRouter
-from django.urls import path, include
 
 
 router = DefaultRouter()
 router.register('post', PostViewSet, basename='posts')
 router.register('comment', CommentViewSet, basename='comments')
+router.register('userInfo', UserInfoViewSet, basename='userInfo')
 urlpatterns = router.urls
--- apis/views.py
+++ apis/views.py
@@ -1,9 +1,13 @@
-from urllib import response
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+
+
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from posts import models as postModels
-from .serializers import PostSerializer, CommentSerializer
+from accounts import models as accountModels
+from .serializers import CustomUserSerializer, PostSerializer, CommentSerializer, UserInfoSerializer
 from datetime import datetime
 
 
@@ -25,7 +29,6 @@ class PostViewSet(viewsets.ModelViewSet):
 
         return Response(responseObject)
 
-
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -38,3 +41,17 @@ class CommentViewSet(viewsets.ModelViewSet):
         else:
             queryset = postModels.Comment.objects.all()
         return queryset
+
+class UserInfoViewSet(viewsets.ModelViewSet):
+    queryset = accountModels.UserInfo.objects.all()
+    serializer_class = UserInfoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    @action(methods=['get'], detail=False, url_path='username/(?P<username>\w+)')
+    def getByUsername(self, request, username):
+        user = get_object_or_404(accountModels.CustomUser, username=username)
+        user_data = CustomUserSerializer(user).data
+        userInfo = get_object_or_404(accountModels.UserInfo, user=user_data['id'])
+        userInfo_data = UserInfoSerializer(userInfo).data
+        return Response(userInfo_data, status=status.HTTP_200_OK)
+
