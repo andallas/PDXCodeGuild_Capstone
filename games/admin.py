e14064a6d88bd8b95d048e4512a0df69f44f703e
Justin <justin.35f@gmail.com>
2022-04-13 18:07:22 -0700

    all gone

--- games/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import GameInfo, GameScore, GameAchievement
-
-
-admin.site.register(GameInfo)
-admin.site.register(GameScore)
-admin.site.register(GameAchievement)
